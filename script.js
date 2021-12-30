068ffa8353cf188b3c9e1b87665a775b7cebcac1
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-12-29 20:21:58 -0500

    initial canvas setup

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+console.log("Hello world");
+
+// Setup canvas / context
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
