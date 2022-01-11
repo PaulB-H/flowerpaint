77582be28f74c41d355a3e3ee437a51017d6bba4
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2022-01-11 18:42:12 -0500

    comment out sound resources & functions

--- public/script.js
+++ public/script.js
@@ -45,48 +45,48 @@ flowerArr.push(yellowflower)
 
 
 // Making sounds
-const soundArr = [];
+// const soundArr = [];
 
-const cartoonpop = new Audio("./sounds/cartoonpop.mp4");
-soundArr.push(cartoonpop)
+// const cartoonpop = new Audio("./sounds/cartoonpop.mp4");
+// soundArr.push(cartoonpop)
 
-const crow = new Audio("./sounds/crow.mp4");
-soundArr.push(crow)
+// const crow = new Audio("./sounds/crow.mp4");
+// soundArr.push(crow)
 
-const hover = new Audio("./sounds/hover.mp4");
-soundArr.push(hover)
+// const hover = new Audio("./sounds/hover.mp4");
+// soundArr.push(hover)
 
-const pop = new Audio("./sounds/pop.mp4");
-soundArr.push(pop)
+// const pop = new Audio("./sounds/pop.mp4");
+// soundArr.push(pop)
 
-const retrolaser = new Audio("./sounds/retrolaser.mp4");
-soundArr.push(retrolaser)
+// const retrolaser = new Audio("./sounds/retrolaser.mp4");
+// soundArr.push(retrolaser)
 
-const scifiui = new Audio("./sounds/scifiui.mp4");
-soundArr.push(scifiui)
+// const scifiui = new Audio("./sounds/scifiui.mp4");
+// soundArr.push(scifiui)
 
-const synthglide = new Audio("./sounds/synthglide.mp4");
-soundArr.push(synthglide)
+// const synthglide = new Audio("./sounds/synthglide.mp4");
+// soundArr.push(synthglide)
 
 
-const getRandLoc = () => {
-  const x = Math.floor(Math.random() * canvas.width);
-  const y = Math.floor(Math.random() * canvas.height);
-  return [x, y];
-}
+// const getRandLoc = () => {
+//   const x = Math.floor(Math.random() * canvas.width);
+//   const y = Math.floor(Math.random() * canvas.height);
+//   return [x, y];
+// }
 
-const placeRandFlower = () => {
-  const randFlower = flowerArr[Math.floor(Math.random() * flowerArr.length)];
+// const placeRandFlower = () => {
+//   const randFlower = flowerArr[Math.floor(Math.random() * flowerArr.length)];
 
-  const randXY = getRandLoc();
+//   const randXY = getRandLoc();
 
-  const imageSize = 150;
+//   const imageSize = 150;
 
-  ctx.drawImage(randFlower, randXY[0] - (imageSize / 2), randXY[1] - (imageSize / 2), imageSize, imageSize);
+//   ctx.drawImage(randFlower, randXY[0] - (imageSize / 2), randXY[1] - (imageSize / 2), imageSize, imageSize);
 
-  stopAllSounds();
-  playRandomSound();
-}
+//   stopAllSounds();
+//   playRandomSound();
+// }
 
 let activeColor = "rainbow";
 
@@ -141,55 +141,55 @@ const placeFlower = (e) => {
 
   ctx.drawImage(randFlower, Xloc - (imageSize / 2), Yloc - (imageSize / 2), imageSize, imageSize);
 
-  stopAllSounds();
+  // stopAllSounds();
   // playRandomSound();
 }
 
-const toggleRandom = () => {
-  const toggleRandBtn = document.getElementById("toggle-random");
+// const toggleRandom = () => {
+//   const toggleRandBtn = document.getElementById("toggle-random");
 
-  if (toggleRandBtn.innerText === "Draw") {
-    canvas.removeEventListener("click", placeRandFlower)
-    canvas.addEventListener("click", placeFlower)
+//   if (toggleRandBtn.innerText === "Draw") {
+//     canvas.removeEventListener("click", placeRandFlower)
+//     canvas.addEventListener("click", placeFlower)
 
-    toggleRandBtn.innerText = "Random"
-  } else {
-    canvas.removeEventListener("click", placeFlower)
-    canvas.addEventListener("click", placeRandFlower)
+//     toggleRandBtn.innerText = "Random"
+//   } else {
+//     canvas.removeEventListener("click", placeFlower)
+//     canvas.addEventListener("click", placeRandFlower)
 
-    toggleRandBtn.innerText = "Draw"
-  }
-}
+//     toggleRandBtn.innerText = "Draw"
+//   }
+// }
 
 canvas.addEventListener("click", placeFlower)
 
-const stopAllSounds = () => {
-  soundArr.forEach((item) => {
-    item.pause()
-    item.currentTime = 0;
-  })
-}
+// const stopAllSounds = () => {
+//   soundArr.forEach((item) => {
+//     item.pause()
+//     item.currentTime = 0;
+//   })
+// }
 
-const playRandomSound = () => {
-  soundArr[Math.floor(Math.random() * soundArr.length)].play()
-}
+// const playRandomSound = () => {
+//   soundArr[Math.floor(Math.random() * soundArr.length)].play()
+// }
 
-const muteAudio = () => soundArr.forEach((sound) => sound.muted = true);
+// const muteAudio = () => soundArr.forEach((sound) => sound.muted = true);
 
-const unMuteAudio = () => soundArr.forEach((sound) => sound.muted = false);
+// const unMuteAudio = () => soundArr.forEach((sound) => sound.muted = false);
 
-const toggleMute = () => {
-  const muteBtn = document.getElementById("toggle-mute");
+// const toggleMute = () => {
+//   const muteBtn = document.getElementById("toggle-mute");
 
-  if (muteBtn.innerText === "Mute") {
-    muteAudio();
-    muteBtn.innerText = "Unmute"
-  } else {
-    unMuteAudio();
-    muteBtn.innerText = "Mute"
-  }
+//   if (muteBtn.innerText === "Mute") {
+//     muteAudio();
+//     muteBtn.innerText = "Unmute"
+//   } else {
+//     unMuteAudio();
+//     muteBtn.innerText = "Mute"
+//   }
 
-}
+// }
 
 const clearCanvas = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
